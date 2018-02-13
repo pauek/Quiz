1733173ca55718b9334f53ad3c6fd8453c5340be
Pau Fernández <pau.fernandez@upc.edu>
2018-02-13 11:54:30 +0100

    Comprovem la resposta.

--- app/src/main/java/fernandez/pau/quiz/QuizActivity.java
+++ app/src/main/java/fernandez/pau/quiz/QuizActivity.java
@@ -2,14 +2,21 @@ package fernandez.pau.quiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
 
     private final int[] ids_botons = {
         R.id.answer1, R.id.answer2, R.id.answer3, R.id.answer4
     };
+
+    private int solution;
+    private RadioGroup answers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,5 +33,45 @@ public class QuizActivity extends AppCompatActivity {
             rb.setText(answer_texts[i]);
         }
 
+        Button btn_check = (Button) findViewById(R.id.btn_check);
+        btn_check.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkAnswer();
+            }
+        });
+
+        solution = getResources().getInteger(R.integer.solution);
+        answers = (RadioGroup) findViewById(R.id.answers);
+    }
+
+    private void checkAnswer() {
+        int id_checked = answers.getCheckedRadioButtonId();
+        int quin = -1;
+        for (int i = 0; i < ids_botons.length; i++) {
+            if (id_checked == ids_botons[i]) {
+                quin = i+1;
+            }
+        }
+        if (quin != -1) {
+            if (quin == solution) {
+                Toast.makeText(this, "Correcte!", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Incorrecte...", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
